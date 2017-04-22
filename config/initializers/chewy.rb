23dd3aa3786803a6bfb0bd8f03f294703f98f34e
Reginaldo Junior <juniorjp1989@gmail.com>
2017-04-22 20:10:15 -0300

    set sidekiq as default strategy to insert ES documents

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,1 +1,5 @@
 Chewy.settings = {host: 'localhost:9200', prefix: 'stock_system'}
+Chewy.root_strategy = :sidekiq
+Chewy::Railtie.console do
+  Chewy.strategy.pop if Chewy.strategy.current.name == :urgent
+end
