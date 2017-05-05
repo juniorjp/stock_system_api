a46874ff95adbc9eb6bacfe2e916d0c56d66ba57
Reginaldo Junior <juniorjp1989@gmail.com>
2017-05-05 17:53:41 -0300

    add create product integration test

---
+++ spec/acceptance/v2/product_spec.rb
@@ -0,0 +1,31 @@
+require 'acceptance_helper'
+
+
+resource 'Product' do
+  header 'Accept', 'application/json'
+  header 'Content-Type', 'application/json'
+  post '/v2/products' do
+    parameter :name, 'Product Name', required: true, scope: :product
+    parameter :quantity, 'Quantity', required: true, scope: :product
+    parameter :price, 'Price', required: true, scope: :product
+    name = 'Ventilador Arno'
+    code = 'A1-2017'
+    quantity = 25
+    price = 70.1
+    let(:raw_post) {  {product: {code: code, name: name, quantity: quantity, price: price, }}.to_json }
+    example_request 'Create a new product' do
+      explanation 'First, create a product, then returns a response with the product registered data'
+      product = JSON.parse(response_body)
+      puts product
+      expect(product).to include({
+         'name' => name,
+         'code' => code,
+         'quantity' => quantity,
+         'price' => price,
+
+      })
+      expect(status).to eq(200)
+    end
+  end
+
+end
