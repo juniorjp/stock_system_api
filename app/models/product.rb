d0ce35434767a4e56d4eea9a7a4e2e6631162f78
Reginaldo Junior <juniorjp1989@gmail.com>
2017-05-05 14:30:18 -0300

    add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :code, presence: true
+  validates :price, presence: true, numericality: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
 end
