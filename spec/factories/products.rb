608d51833346afde6141a0d72272c219189d4cd7
Reginaldo Junior <juniorjp1989@gmail.com>
2017-05-05 14:26:06 -0300

    create failing product tests and factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :product do
-    
+    name 'Herbalife'
+    price 25.7
+    code 001
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  #presence validation
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:price) }
+  it { should validate_presence_of(:code) }
+
+  #numericality validation
+  it { should validate_numericality_of(:price) }
+  it { should validate_numericality_of(:quantity) }
+
+  #business validation
+  it 'must have a positive price' do
+    product  = FactoryGirl.build(:product, price: 0)
+    expect(product).to_not be_valid
+    product.price = 27
+    expect(product).to be_valid
+  end
 end
