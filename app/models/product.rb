811fdbf4686f5d6ca147e1f0b14f8aa36dd75bed
Reginaldo Junior <juniorjp1989@gmail.com>
2017-05-06 00:40:31 -0300

    create categories n..n relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: true
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
+  has_many :categories, through: :categories_products, class_name: 'CategoryProduct'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506032504) do
+ActiveRecord::Schema.define(version: 20170506033023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170506032504) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "product_id",  null: false
+    t.integer "category_id", null: false
+    t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id", using: :btree
+    t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
