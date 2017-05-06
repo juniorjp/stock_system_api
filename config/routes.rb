3d697b0f33dcf0ca9383f3924f500e47805d5541
Reginaldo Junior <juniorjp1989@gmail.com>
2017-05-05 21:10:22 -0300

    change index to search resource(post)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :v2, defaults: { format: :json } do
     resources :products do
+      post :index, path: '/search', on: :collection
     end
   end
 end
